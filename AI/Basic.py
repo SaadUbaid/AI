3bff19ede65c380e01695832e07f154449ce03dc
Saad Ubaid <saadubaid47@gmail.com>
2025-06-23 20:09:01 +0500

    Add files via upload

---
+++ AI/Basic.py
@@ -0,0 +1,65 @@
+#print function
+print("hello world")
+ 
+#Data types
+# Integer
+print(9)
+print(1e309)
+#floaat
+print(1.2)
+print(1.7e309)
+#boolean
+print(True)
+print(False)
+#string
+print("hello world")
+#complex number
+print(1+5j)
+#list
+print([1, 2, 3, 4, 5])
+#tuple
+print((1, 2, 3, 4, 5))
+#set
+print({1, 2, 3, 4, 5})
+#dictionary
+print({"name": "Saad", "age": 21, "city": "pakistan"})
+#type
+type(3)
+type(3.5)
+type("hello world")
+
+#variable
+name = "Saad"
+print(name)
+a = 4
+b =8
+print(a + b)
+#stylistic variable naming
+a,b,c = 1, 2, 3
+print(a, b, c)
+a=b=c = 5
+print(a, b, c)
+
+#dynamic typing
+a = 4
+#static typing
+inta = 4
+
+#keyword and identifiers
+#keywords
+#identifiers
+# 1 you can not start with digit
+# 2 you can use  uppercase and lowercase letters and special chars is _
+# 3 you can not use keywords as identifiers
+
+#type conversion
+#implicit conversion
+print(5+5.6)
+#explicit conversion
+# string to int
+int(2)
+# int to string
+str(2)
+# float to int
+float(2)
+
---
+++ AI/basic_code.py
@@ -0,0 +1,9 @@
+#user input
+# take input from user and store them in a variables
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+# add the 2 variables
+result = num1+num2
+#print the result
+print(result)
+
